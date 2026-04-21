b19368d6e502099db4fa9d8e7d984bfc4e226409
estebanmenendez <e.menendez19@gmail.com>
2026-04-21 16:06:19 -0300

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,21 +1,34 @@
-export const headerData = {
-  links: [
-    { text: 'Inicio', href: '/' },
-    { text: 'Quiénes Somos', href: '/#quienes-somos' },
-    { text: 'Pilares', href: '/#pilares' },
-    { text: 'Contacto', href: '/#contacto' },
-  ],
-  actions: [{ text: 'Unirse', href: '#contacto', variant: 'primary' }],
-};
-
 export const footerData = {
-  secondaryLinks: [
-    { text: 'Libertad', href: '#' },
-    { text: 'Fraternidad', href: '#' },
-    { text: 'Igualdad', href: '#' },
+  links: [
+    {
+      title: 'Capítulo',
+      links: [
+        { text: 'Historia', href: '#quienes-somos' },
+        { text: 'Nuestro Credo', href: '#quienes-somos' },
+        { text: 'Pilares', href: '#pilares' },
+      ],
+    },
+    {
+      title: 'Comunidad',
+      links: [
+        { text: 'Eventos', href: '#eventos' },
+        { text: 'Membresía', href: '#contacto' },
+      ],
+    },
   ],
-  socialLinks: [],
-  footNote: `Widows Sons MRA - Capítulo Rio Uruguay`,
+  secondaryLinks: [
+    { text: 'Términos y Condiciones', href: '/terms' },
+    { text: 'Privacidad', href: '/privacy' },
+  ],
+  socialLinks: [
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: '#' },
+  ],
+  footNote: `
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm"></span>
+    Diseñado por <a class="text-blue-600 underline dark:text-muted" href="#"> Esteban Menendez</a> · Widows Sons Rio Uruguay © 2026
+  `,
 };
 /*import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
