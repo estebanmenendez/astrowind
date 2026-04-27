94d56128d9da34b79744211b3691cf58f74c5593
estebanmenendez <e.menendez19@gmail.com>
2026-04-27 14:31:35 -0300

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,4 +1,5 @@
-// 1. Asegúrate de que exista headerData (el menú de arriba)
+import { getPermalink, getBlogPermalink } from './utils/permalinks';
+
 export const headerData = {
   links: [
     {
@@ -7,294 +8,48 @@ export const headerData = {
     },
     {
       text: 'Quiénes Somos',
-      href: '/#quienes-somos',
-    },
-    {
-      text: 'Pilares',
-      href: '/#pilares',
+      href: getPermalink('/about'), // AHORA SÍ: Apunta a tu nueva página institucional
     },
     {
       text: 'Eventos',
-      href: '/#eventos',
+      href: getBlogPermalink(), // Te lleva al listado de eventos que estuvimos arreglando
     },
     {
       text: 'Contacto',
-      href: '/#contacto',
+      href: '/#contacto', // Este sí puede ser un ancla al formulario de abajo
     },
   ],
-  actions: [{ text: 'Unirse', href: '#contacto', variant: 'primary' }],
+  actions: [{ text: 'Unirse', href: '/#contacto', variant: 'primary' }],
 };
 
-// 2. Asegúrate de que tu nuevo footerData también tenga el export
 export const footerData = {
   links: [
     {
       title: 'Capítulo',
       links: [
-        { text: 'Historia', href: '#' },
-        { text: 'Nuestros Pilares', href: '/pilares' },
-        { text: 'Autoridades', href: '#' },
+        { text: 'Quiénes Somos', href: getPermalink('/about') },
+        { text: 'Eventos', href: getBlogPermalink() },
+        { text: 'Membresía', href: '/#contacto' },
       ],
     },
     {
       title: 'Comunidad',
       links: [
-        { text: 'Próximos Eventos', href: '/blog' },
-        { text: 'Acción Social', href: '#' },
-        { text: 'Membresía', href: '#' },
-      ],
-    },
-    {
-      title: 'Contacto',
-      links: [
-        { text: 'Escríbenos', href: '/contacto' },
-        { text: 'Ubicación', href: '#' },
+        { text: 'Instagram', href: 'https://instagram.com/tu_cuenta' },
+        { text: 'Facebook', href: 'https://facebook.com/tu_cuenta' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Términos y Condiciones', href: '#' },
-    { text: 'Privacidad', href: '#' },
+    { text: 'Fraternidad', href: '#' },
+    { text: 'Servicio', href: '#' },
+    { text: 'Honor', href: '#' },
   ],
   socialLinks: [
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/tu_cuenta' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://facebook.com/tu_cuenta' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm"></span>
     Diseñado por <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://github.com/estebanmenendez"> Esteban Menéndez</a> · Widows Sons Rio Uruguay © 2026
   `,
 };
-/*import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
-
-export const headerData = {
-  links: [
-    {
-      text: 'Inicio',
-      href: '/',
-    },
-    {
-      text: 'Quiénes Somos',
-      href: '/#quienes-somos',
-    },
-    {
-      text: 'Pilares',
-      href: '/#pilares',
-    },
-    {
-      text: 'Galería',
-      href: '/#galeria',
-    },
-    {
-      text: 'Contacto',
-      href: '/#contacto',
-    },
-  ],
-  actions: [{ 
-    text: 'Unirse', 
-    href: '#contacto', 
-    variant: 'primary',
-  }],
-  
-  /*links: [
- 
-    {
-      text: 'Inicio',
-      
-    
-   *links: [
-        {
-          text: 'Quienes Somos',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Nuestro Pilares',
-          href: getPermalink('/homes/startup'),
-        },
-        
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
-    },
-    {
-      text: 'Filantropia',
-      links: [
-        {
-          text: 'Nuestras obras (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-       {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Eventos',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-         {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
-  ],
-  actions: [{ text: 'Download', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
-};*/
-/*
-export const footerData = {
-  secondaryLinks: [
-    { text: 'Libertad', href: '#' },
-    { text: 'Fraternidad', href: '#' },
-    { text: 'Igualdad', href: '#' },
-  ],
-  socialLinks: [
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-  ],
-  footNote: `
-    Widows Sons — Capítulo Rio Uruguay · Todos los derechos reservados.
-  `,
-};
-/*export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
-  socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
-  ],
-  footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
-  `,
-};
-*/
